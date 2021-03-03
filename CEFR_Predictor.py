077d71944e05f4f86a6fda2b9117414e2e159206
Adam Montgomerie <>
2021-03-03 16:01:42 +0900

    add support for uploading text files in streamlit app

--- CEFR_Predictor.py
+++ CEFR_Predictor.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from io import StringIO
 
 from cefr_predictor.inference import Model
 
@@ -12,28 +13,52 @@ def load_model():
 def app():
     st.write("# English CEFR Level Predictor")
 
-    text = st.text_area("Type or paste a text here:", height=200)
+    textbox_text = st.text_area("Type or paste a text here:", height=200)
 
-    if text:
-        output = predict([text])
-        display_results(output)
+    uploaded_files = st.file_uploader(
+        "Or choose one or more text files to upload",
+        type=["txt"],
+        accept_multiple_files=True,
+    )
+
+    if st.button("Predict"):
+        input_texts = collect_inputs(textbox_text, uploaded_files)
+
+        if input_texts:
+            output = model.predict_decode(input_texts)
+            display_results(input_texts, output)
+
+        else:
+            st.write("Input one or more texts to generate a prediction.")
 
     st.write(
         "For more information: [amontgomerie.github.io](https://amontgomerie.github.io)"
     )
 
 
-def predict(textlist):
-    preds, probas = model.predict_decode(textlist)
+def collect_inputs(textbox_text, uploaded_files):
+    inputs = []
 
-    return {"level": preds[0], "scores": probas[0]}
+    if textbox_text:
+        inputs.append(textbox_text)
+
+    if uploaded_files:
+        for upload in uploaded_files:
+            stringio = StringIO(upload.getvalue().decode("utf-8"))
+            text = stringio.read()
+            inputs.append(text)
+
+    return inputs
 
 
-def display_results(results):
-    st.write(f"## Predicted CEFR level: __{results['level']}__")
-
-    st.write("Text score per level:")
-    st.write(results["scores"])
+def display_results(texts, output):
+    levels, scores = output
+    for i, (text, level, score) in enumerate(zip(texts, levels, scores)):
+        st.write(f"### Text {i+1}:")
+        st.write(f"_{text}_")
+        st.write(f"### Predicted CEFR level: __{level}__")
+        st.write("### Text score per level:")
+        st.write(score)
 
 
 if __name__ == "__main__":
